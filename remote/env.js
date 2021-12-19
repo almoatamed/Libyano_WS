c6bf377b3ef9342160b694392524ae3d9b16f798
Salem Elmotamed <s.elmotamed96@gmail.com>
2021-12-19 20:01:27 +0200

    main

--- remote/env.js
+++ remote/env.js
@@ -3,8 +3,8 @@ export default {
     bridge_address:'10.0.0.1',
     api_port:'80',
 
-    // server_addr: '192.168.55.1',
-    // bridge_address:'192.168.55.1',
+    // server_addr: 'localhost',
+    // bridge_address:'localhost',
     // api_port:'8085',
     
 }
